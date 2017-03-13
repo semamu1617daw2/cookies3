a2a3eb957c3260e93035d6c895fc6402e170db83
semamu1617daw2 <semamu1617daw2@gmail.com>
2017-03-13 21:12:06 +0100

    PHP cookies3

--- index.php
+++ index.php
@@ -1,16 +1,24 @@
-<html> 
-    <head>  
-        <title>Desplegant una aplicació PHP sobre Heroku</title> 
-    </head> 
-    <body> 
-        <?php 
-            echo "Afegint Cookies<br>"; 
-            echo "Sóc el desenvolupador semammu1617daw2"; 
-            setcookie("nom", "Jaume Pons", time()+3600, "/", "", 0); 
-            setcookie("edat", "20", time()+3600, "/", "", 0); 
-            setcookie("cicle", "DAW", time()+3600, "/", "", 0); 
-            setcookie("curs", "2n", time()+3600, "/", "", 0); 
-            setcookie("any", "2016­2017", time()+3600, "/", "", 0);            
-        ?>  
-    </body>    
-</html> 
+
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Desplegant una aplicació PHP sobre Heroku</title>
+    </head>
+    <body>
+        <?php
+            echo "Afegint Cookies<br>";
+            echo "Sóc el desenvolupador semamu1617daw2<br>";
+            setcookie("name", "Jaume Pons", time()+3600, "/", "", 0);               
+            setcookie("edat", "20", time()+3600, "/", "", 0);
+            setcookie("cicle", "daw", time()+3600, "/", "", 0);
+            setcookie("curs", "2n", time()+3600, "/", "", 0);
+            setcookie("any", "2016-17", time()+3600, "/", "", 0);
+        ?>
+    </body>
+</html>
